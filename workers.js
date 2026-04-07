7268c18284ddc24df4084ae670606155138988ca
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-06 18:03:21 -0700

    Update workers.js
    const myWorker = new Worker("/worker.js");
    const first = document.querySelector("input#number1");
    const second = document.querySelector("input#number2");
    
    first.onchange = () => {
      myWorker.postMessage([first.value, second.value]);
      console.log("Message posted to worker");
    };
    
    onmessage = (e) => {
      console.log("Worker: Message received from main script");
    
      const result = e.data[0] * e.data[1];
    
      if (isNaN(result)) {
        postMessage("Please write two numbers");
      } else {
        const workerResult = "Result: " + result;
        console.log("Worker: Posting message back to main script");
        postMessage(workerResult);
      }
    };

--- workers.js
+++ workers.js
@@ -1,1 +1,22 @@
+const myWorker = new Worker("/worker.js");
+const first = document.querySelector("input#number1");
+const second = document.querySelector("input#number2");
 
+first.onchange = () => {
+  myWorker.postMessage([first.value, second.value]);
+  console.log("Message posted to worker");
+};
+
+onmessage = (e) => {
+  console.log("Worker: Message received from main script");
+
+  const result = e.data[0] * e.data[1];
+
+  if (isNaN(result)) {
+    postMessage("Please write two numbers");
+  } else {
+    const workerResult = "Result: " + result;
+    console.log("Worker: Posting message back to main script");
+    postMessage(workerResult);
+  }
+};
