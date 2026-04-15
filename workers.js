858470c8e7c9d11df95d5254b44710ffdf9932c4
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-15 01:59:33 -0700

    Add binary search function to workers.js

--- workers.js
+++ workers.js
@@ -20,3 +20,24 @@ onmessage = (e) => {
     postMessage(workerResult);
   }
 };
+
+
+
+// query database
+
+function binarySearch(arr, el, compare_fn) {
+    let m = 0;
+    let n = arr.length - 1;
+    while (m <= n) {
+        let k = (n + m) >> 1;
+        let cmp = compare_fn(el, arr[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return ~m;
+}
