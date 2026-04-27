5a0e3cc37adcd1cbb9d37b0cf6664617ab41a7d1
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-26 23:10:25 -0700

    Update buildlink3.mk

--- pkgsrc/databases/postgresql14-client/buildlink3.mk
+++ pkgsrc/databases/postgresql14-client/buildlink3.mk
@@ -1,1 +1,41 @@
+# $NetBSD: buildlink3.mk,v 1.8 2026/04/17 09:38:54 adam Exp $
 
+BUILDLINK_TREE+=	postgresql14-client
+
+.if !defined(POSTGRESQL14_CLIENT_BUILDLINK3_MK)
+POSTGRESQL14_CLIENT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql14-client+=	postgresql14-client>=14.0
+BUILDLINK_ABI_DEPENDS.postgresql14-client+=	postgresql14-client>=14.17nb2
+BUILDLINK_PKGSRCDIR.postgresql14-client?=	../../databases/postgresql14-client
+
+# This variable contains the libraries need to link most clients.
+BUILDLINK_LDADD.postgresql14-client+=	-lpq ${BUILDLINK_LDADD.gettext}
+BUILDLINK_FILES.postgresql14-client+=	bin/pg_config
+
+pkgbase := postgresql14-client
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql14-client:Mlz4}
+.include "../../archivers/lz4/buildlink3.mk"
+.endif
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} == "SunOS"
+.include "../../devel/ossp-uuid/buildlink3.mk"
+.endif
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+
+.if ${PKG_BUILD_OPTIONS.postgresql14-client:Mgssapi}
+.  include "../../mk/krb5.buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.postgresql14-client:Micu}
+.  include "../../textproc/icu/buildlink3.mk"
+.endif
+.endif # POSTGRESQL14_CLIENT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql14-client
